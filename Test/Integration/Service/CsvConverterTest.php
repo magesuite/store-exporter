418faf3dc6cdf7f79324c0261f31372213e70947
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2021-03-04 13:49:22 +0100

    tests: Adding a fix to tests to allow executing it in projects with custom sources added from code

--- Test/Integration/Service/CsvConverterTest.php
+++ Test/Integration/Service/CsvConverterTest.php
@@ -12,32 +12,26 @@ class CsvConverterTest extends \PHPUnit\Framework\TestCase
     /**
      * @var \MageSuite\StoreExporter\Service\ConverterFactory
      */
-    private $coverterFactory;
+    protected $converterFactory;
 
     /**
      * @var \Magento\Inventory\Model\ResourceModel\Source\CollectionFactory
      */
-    private $collectionFactory;
+    protected $collectionFactory;
 
     /**
      * @var \Magento\Framework\Filesystem
      */
     protected $filesystem;
 
-    /**
-     * @var \Magento\Framework\File\Csv
-     */
-    protected $csvProcessor;
-
     public function setUp(): void
     {
         parent::setUp();
 
         $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $this->coverterFactory = $this->objectManager->get(\MageSuite\StoreExporter\Service\ConverterFactory::class);
+        $this->converterFactory = $this->objectManager->get(\MageSuite\StoreExporter\Service\ConverterFactory::class);
         $this->collectionFactory = $this->objectManager->get(\Magento\Inventory\Model\ResourceModel\Source\CollectionFactory::class);
         $this->filesystem = $this->objectManager->get(\Magento\Framework\Filesystem::class);
-        $this->csvProcessor = $this->objectManager->get(\Magento\Framework\File\Csv::class);
     }
 
     /**
@@ -48,14 +42,15 @@ class CsvConverterTest extends \PHPUnit\Framework\TestCase
      */
     public function testExport()
     {
-        $this->markTestSkipped();
-        $csvConverter = $this->coverterFactory->create();
+        $csvConverter = $this->converterFactory->create();
         $inventorySourceCollection = $this->collectionFactory->create();
         $csvConverter->convert($inventorySourceCollection);
         $filePath = $csvConverter->getFilePath();
-        $data = $this->csvProcessor->getData($filePath);
+        $data = file_get_contents($filePath);
 
-        $this->assertEquals($data, $this->getExpectedResult());
+        foreach ($this->getExpectedSourcesData() as $index => $item) {
+            $this->assertContains($item, $data);
+        }
 
         $dir = $this->filesystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::VAR_DIR);
         $dir->delete($filePath);
@@ -66,176 +61,16 @@ class CsvConverterTest extends \PHPUnit\Framework\TestCase
         require __DIR__ . '/../../_files/sources.php';
     }
 
-    private function getExpectedResult(){
+    protected function getExpectedSourcesData()
+    {
         return [
-            [
-                "source_code",
-                "name",
-                "enabled",
-                "description",
-                "latitude",
-                "longitude",
-                "country_id",
-                "region_id",
-                "region",
-                "city",
-                "street",
-                "postcode",
-                "contact_name",
-                "email",
-                "phone",
-                "fax",
-                "use_default_carrier_config",
-                "is_pickup_location_active",
-                "frontend_name",
-                "frontend_description",
-                "url",
-                "carrier_links"
-            ],
-            [
-                "default",
-                "Default Source",
-                "1",
-                "Default Source",
-                "0.000000",
-                "0.000000",
-                "US",
-                "",
-                "",
-                "",
-                "",
-                "00000",
-                "",
-                "",
-                "",
-                "",
-                "1",
-                "0",
-                "",
-                "",
-                "",
-                ""
-            ],
-            [
-                "eu-1",
-                "EU-source-1",
-                "1",
-                "",
-                "",
-                "",
-                "FR",
-                "",
-                "",
-                "",
-                "",
-                "postcode",
-                "",
-                "",
-                "",
-                "",
-                "1",
-                "0",
-                "",
-                "",
-                "",
-                ""
-            ],
-            [
-                "eu-2",
-                "EU-source-2",
-                "1",
-                "",
-                "",
-                "",
-                "FR",
-                "",
-                "",
-                "",
-                "",
-                "postcode",
-                "",
-                "",
-                "",
-                "",
-                "1",
-                "0",
-                "",
-                "",
-                "",
-                ""
-            ],
-            [
-                "eu-3",
-                "EU-source-3",
-                "1",
-                "",
-                "",
-                "",
-                "DE",
-                "",
-                "",
-                "",
-                "",
-                "postcode",
-                "",
-                "",
-                "",
-                "",
-                "1",
-                "0",
-                "",
-                "",
-                "",
-                ""
-            ],
-            [
-                "eu-disabled",
-                "EU-source-disabled",
-                "0",
-                "",
-                "",
-                "",
-                "DE",
-                "",
-                "",
-                "",
-                "",
-                "postcode",
-                "",
-                "",
-                "",
-                "",
-                "1",
-                "0",
-                "",
-                "",
-                "",
-                "",
-            ],
-            [
-                "us-1",
-                "US-source-1",
-                "1",
-                "",
-                "",
-                "",
-                "US",
-                "",
-                "",
-                "",
-                "",
-                "postcode",
-                "",
-                "",
-                "",
-                "",
-                "1",
-                "0",
-                "",
-                "",
-                "",
-                ""
-            ]
+            'source_code,name,enabled,description,latitude,longitude,country_id,region_id,region,city,street,postcode',
+            'default,"Default Source",1,"Default Source",0.000000,0.000000,US,,,,,00000',
+            'eu-1,EU-source-1,1,,,,FR,,,,,postcode',
+            'eu-2,EU-source-2,1,,,,FR,,,,,postcode',
+            'eu-3,EU-source-3,1,,,,DE,,,,,postcode',
+            'eu-disabled,EU-source-disabled,0,,,,DE,,,,,postcode',
+            'us-1,US-source-1,1,,,,US,,,,,postcode'
         ];
     }
 }
